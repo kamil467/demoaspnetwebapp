820de84b0fd63e75f12adb606a5856ce61a5cfbd
Kamil Hussain <31802480+kamil467@users.noreply.github.com>
2025-07-05 09:33:40 +0300

    Add files via upload

---
+++ Repository/FontyDbContext.cs
@@ -0,0 +1,22 @@
+using DemoWebApplication.Model;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
+
+namespace DemoWebApplication.Repository;
+
+public class FontyDbContext: DbContext
+{
+    public DbSet<Region>  Regions { get; set; }
+
+    public FontyDbContext(DbContextOptions<FontyDbContext> options)
+    : base (options){
+        
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Region>()
+            .HasKey(r => r.Id);
+    }
+    
+}
